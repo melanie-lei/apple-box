1d8bbbe527e7a70930ce01cf43e38d926b220f1f
melanie-lei <65362046+melanie-lei@users.noreply.github.com>
2025-07-27 16:45:50 -0400

    Rectangles

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ for i in range(NUM_APPLES_Y):
 
 
 def move_mouse(from_x, from_y, to_x, to_y):
+    
     if to_x > from_x: # dragging to the right
         from_pixel_x = TOP_LEFT_X + from_x * APPLE_WIDTH - 5
         to_pixel_x = TOP_LEFT_X + to_x * APPLE_WIDTH + APPLE_WIDTH + 7
@@ -84,6 +85,16 @@ def move_mouse(from_x, from_y, to_x, to_y):
     multiplier = max(abs(from_x - to_x), abs(from_y - to_y))
     auto.dragTo(to_pixel_x, to_pixel_y, .3*multiplier, auto.easeOutQuad, button="left")
 
+    x = min(from_x, to_x)
+    ox = max(from_x, to_x)
+    y = min(from_y, to_y)
+    oy = max(from_y, to_y)
+
+    for i in range(x, ox + 1): # set all between to 0
+        for j in range(y, oy + 1):
+                board_map[j][i] = 0
+    return True
+
 
 def pair_ten_algorithm(x, y):
     # return if theres no apple
@@ -91,6 +102,7 @@ def pair_ten_algorithm(x, y):
         return False
     
     curr_num = board_map[y][x]
+    #print(curr_num)
 
     # check all surrounding apples if add up to 10
     # right -> down -> left -> up
@@ -100,11 +112,24 @@ def pair_ten_algorithm(x, y):
     while temp_x < 17:
         temp_sum += board_map[temp_y][temp_x]
         if temp_sum == 10: # makes 10
-            move_mouse(x, y, temp_x, temp_y)
-            for i in range(x, temp_x + 1): # set all between to 0
-                board_map[y][i] = 0
-            return True
+            return move_mouse(x, y, temp_x, temp_y)
         elif temp_sum > 10: # 10 isnt possible
+            temp_sum -= board_map[temp_y][temp_x]
+            temp_x-=1
+            temp_y+=1
+            while temp_y < 10 and temp_x > x:
+                for i in range(x, temp_x + 1): # sum all in new row
+                    temp_sum += board_map[temp_y][i]
+                if temp_sum == 10:
+                    return move_mouse(x, y, temp_x, temp_y)
+                if temp_sum > 10:
+                    while temp_sum > 10:
+                        for i in range(y, temp_y + 1): # sub all in excess row
+                            temp_sum -= board_map[i][temp_x]
+                        temp_x-=1
+                        if temp_sum == 10:
+                            return move_mouse(x, y, temp_x, temp_y)
+                temp_y+=1
             break
         else: # sum is less than 10
             temp_x+=1
@@ -115,131 +140,84 @@ def pair_ten_algorithm(x, y):
     while temp_y < 10:
         temp_sum += board_map[temp_y][temp_x]
         if temp_sum == 10: # makes 10
-            move_mouse(x, y, temp_x, temp_y)
-            for i in range(y, temp_y + 1): # set all between to 0
-                board_map[i][x] = 0
-            return True
+            return move_mouse(x, y, temp_x, temp_y)
         elif temp_sum > 10: # 10 isnt possible
+            temp_sum -= board_map[temp_y][temp_x]
+            temp_x-=1
+            temp_y-=1
+            while temp_x >= 0 and temp_y > y:
+                for i in range(y, temp_y + 1): # sum all in new row
+                    temp_sum += board_map[i][temp_x]
+                if temp_sum == 10:
+                    return move_mouse(x, y, temp_x, temp_y)
+                if temp_sum > 10:
+                    while temp_sum > 10:
+                        for i in range(temp_x, x + 1): # sub all in excess row
+                            temp_sum -= board_map[temp_y][i]
+                        temp_y-=1
+                        if temp_sum == 10:
+                            return move_mouse(x, y, temp_x, temp_y)
+                temp_x-=1
             break
         else: # sum is less than 10
             temp_y+=1
-    
+
+
     temp_x = x - 1
     temp_y = y
     temp_sum = curr_num
     while temp_x >= 0:
         temp_sum += board_map[temp_y][temp_x]
         if temp_sum == 10: # makes 10
-            move_mouse(x, y, temp_x, temp_y)
-            for i in range(temp_x, x + 1): # set all between to 0
-                board_map[y][i] = 0
-            return True
+            return move_mouse(x, y, temp_x, temp_y)
         elif temp_sum > 10: # 10 isnt possible
+            temp_sum -= board_map[temp_y][temp_x]
+            temp_x+=1
+            temp_y-=1
+            while temp_y >=0 and temp_x < x:
+                for i in range(temp_x, x + 1): # sum all in new row
+                    temp_sum += board_map[temp_y][i]
+                if temp_sum == 10:
+                    return move_mouse(x, y, temp_x, temp_y)
+                if temp_sum > 10:
+                    while temp_sum > 10:
+                        for i in range(temp_y, y + 1): # sub all in excess row
+                            temp_sum -= board_map[i][temp_x]
+                        temp_x+=1
+                        if temp_sum == 10:
+                            return move_mouse(x, y, temp_x, temp_y)
+                temp_y-=1
             break
         else: # sum is less than 10
             temp_x-=1
-    
+
     temp_x = x
     temp_y = y - 1
     temp_sum = curr_num
     while temp_y >= 0:
         temp_sum += board_map[temp_y][temp_x]
         if temp_sum == 10: # makes 10
-            move_mouse(x, y, temp_x, temp_y)
-            for i in range(temp_y, y + 1): # set all between to 0
-                board_map[i][x] = 0
-            return True
+            return move_mouse(x, y, temp_x, temp_y)
         elif temp_sum > 10: # 10 isnt possible
-            break
-        else: # sum is less than 10
-            temp_y-=1
-    
-    temp_x = x + 1
-    temp_y = y + 1
-    temp_sum = curr_num
-    while temp_x < 17 and temp_y < 10:
-        for i in range(x, temp_x):
-            temp_sum += board_map[temp_y][i]
-        for j in range(y, temp_y):
-            temp_sum += board_map[j][temp_x]
-        temp_sum += board_map[temp_y][temp_x]
-        if temp_sum == 10: # makes 10
-            move_mouse(x, y, temp_x, temp_y)
-            for j in range(y, temp_y + 1):
-                for i in range(x, temp_x + 1): # set all between to 0
-                    board_map[j][i] = 0
-            return True
-        elif temp_sum > 10: # 10 isnt possible
-            break
-        else: # sum is less than 10
+            temp_sum -= board_map[temp_y][temp_x]
             temp_x+=1
             temp_y+=1
-    
-    temp_x = x + 1
-    temp_y = y - 1
-    temp_sum = curr_num
-    while temp_x < 17 and temp_y >= 0:
-        for i in range(x, temp_x):
-            temp_sum += board_map[temp_y][i]
-        for j in range(temp_y + 1, y + 1):
-            temp_sum += board_map[j][temp_x]
-        temp_sum += board_map[temp_y][temp_x]
-        if temp_sum == 10: # makes 10
-            move_mouse(x, y, temp_x, temp_y)
-            for j in range(temp_y, y + 1):
-                for i in range(x, temp_x + 1): # set all between to 0
-                    board_map[j][i] = 0
-            return True
-        elif temp_sum > 10: # 10 isnt possible
+            while temp_x < 17 and temp_y < y:
+                for i in range(temp_y, y + 1): # sum all in new row
+                    temp_sum += board_map[i][temp_x]
+                if temp_sum == 10:
+                    return move_mouse(x, y, temp_x, temp_y)
+                if temp_sum > 10:
+                    while temp_sum > 10:
+                        for i in range(x, temp_x + 1): # sub all in excess row
+                            temp_sum -= board_map[temp_y][i]
+                        temp_y+=1
+                        if temp_sum == 10:
+                            return move_mouse(x, y, temp_x, temp_y)
+                temp_x+=1
             break
         else: # sum is less than 10
-            temp_x+=1
             temp_y-=1
-
-    
-    temp_x = x - 1
-    temp_y = y + 1
-    temp_sum = curr_num
-    while temp_x >= 0 and temp_y < 10:
-        for i in range(temp_x + 1, x + 1):
-            temp_sum += board_map[temp_y][i]
-        for j in range(y, temp_y):
-            temp_sum += board_map[j][temp_x]
-        temp_sum += board_map[temp_y][temp_x]
-        if temp_sum == 10: # makes 10
-            move_mouse(x, y, temp_x, temp_y)
-            for j in range(y, temp_y + 1):
-                for i in range(temp_x, x + 1): # set all between to 0
-                    board_map[j][i] = 0
-            return True
-        elif temp_sum > 10: # 10 isnt possible
-            break
-        else: # sum is less than 10
-            temp_x-=1
-            temp_y+=1
-
-
-    temp_x = x - 1
-    temp_y = y - 1
-    temp_sum = curr_num
-    while temp_x >= 0 and temp_y >= 0:
-        for i in range(temp_x + 1, x + 1):
-            temp_sum += board_map[temp_y][i]
-        for j in range(temp_y + 1, y + 1):
-            temp_sum += board_map[j][temp_x]
-        temp_sum += board_map[temp_y][temp_x]
-        if temp_sum == 10: # makes 10
-            move_mouse(x, y, temp_x, temp_y)
-            for j in range(temp_y, y + 1):
-                for i in range(temp_x, x + 1): # set all between to 0
-                    board_map[j][i] = 0
-            return True
-        elif temp_sum > 10: # 10 isnt possible
-            break
-        else: # sum is less than 10
-            temp_x-=1
-            temp_y-=1
-
     return False
 
 # while timer is not out
